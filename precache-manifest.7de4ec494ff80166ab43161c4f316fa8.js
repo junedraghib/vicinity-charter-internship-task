1a9ded628c5479de788cf38c8aa4886ecdf450e1
j-github01 <raghibjuned@gmail.com>
2019-05-05 04:21:07 +0530

    Updates

---
+++ precache-manifest.7de4ec494ff80166ab43161c4f316fa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b86a17d5d4df4f073abc283dbe98e2",
+    "url": "/contact-app/index.html"
+  },
+  {
+    "revision": "dfecd3864126331b4065",
+    "url": "/contact-app/static/css/main.e085e674.chunk.css"
+  },
+  {
+    "revision": "ca7595794f71adcfd358",
+    "url": "/contact-app/static/js/2.f871b04c.chunk.js"
+  },
+  {
+    "revision": "dfecd3864126331b4065",
+    "url": "/contact-app/static/js/main.ec50ce96.chunk.js"
+  },
+  {
+    "revision": "c728df55cf174d850624",
+    "url": "/contact-app/static/js/runtime~main.46338a14.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c351e70406b7b22bb0d5b4d52ee71d31.js"
+  "/contact-app/precache-manifest.7de4ec494ff80166ab43161c4f316fa8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contact-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
